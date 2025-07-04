1190a8f572d7785ceaad7dde2d8c49555ced3a23
20072008andressa <142400446+20072008andressa@users.noreply.github.com>
2025-07-04 07:48:02 -0300

    ok

--- main.js
+++ main.js
@@ -71,6 +71,7 @@ let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
 function classificaSenha(){
+ let entropia = tamanhoSenha * Math.log2(alfabeto.length);
 forcaSenha.classList.remove('fraca','media','forte');
 if (tamanhoSenha > 11){
 forcaSenha.classList.add('forte');
